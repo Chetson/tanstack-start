a7e59a93eec64050bcd58357e2d88e66089e52e2
chetson <kirgorlov@yandex.ru>
2026-05-30 12:13:52 +0500

    Install Tanstack start libs

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: Home,
+});
+
+function Home() {
+  return <div className="bg-red-500 text-white p-4">Hello World</div>;
+}
